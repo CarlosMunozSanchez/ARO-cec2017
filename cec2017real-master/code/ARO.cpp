b0d9bb8104874d62c1b2fcd480564e5559c41c6a
Carlos Muñoz <mscarlos@correo.ugr.es>
2023-06-25 16:19:25 +0200

    v4&v5 : chaotic operators

--- cec2017real-master/code/ARO.cpp
+++ cec2017real-master/code/ARO.cpp
@@ -33,6 +33,9 @@ int dim = 10;
 //init. seed
 std::mt19937 gen(seed); 
 
+
+// Mutation as the the sum of values following a normal distribution
+/*
 void Mutate(vector<double> & v){
 	// a random number following a normal distribution is added to every component
 	for(int i = 0; i < dim; i++){
@@ -47,6 +50,49 @@ void Mutate(vector<double> & v){
 		}
 	}
 }
+*/
+
+
+// Mutation with chaotic sequences for exploration and exploitation
+void Mutate(vector<double> & v){
+
+	// variable for chaotic sequence
+	static double r = Random::get(-1.0, 1.0);
+	
+	// iteration control
+	static int iter =  0;
+	static const int k = 0.48 * dim * 10000;
+	
+	// Exploration performed at early stages
+	if(iter < k){	
+		for(int i = 0; i < dim; i++){
+			r = sin(20/r);
+			double max = upperBound - v[i] > v[i] - lowerBound ? upperBound - v[i] : v[i] - lowerBound;
+			v[i] += r * max;
+		}
+	}
+	// Exploitation performed at late stages
+	else{
+		for(int i = 0; i < dim; i++){
+			r = sin(20/r);
+			double a = pow((10000 * dim - iter) / 10000 * dim, 2.0); 
+			v[i] += r * (upperBound - lowerBound) * a / 2;
+		}
+	}
+	
+	// values are truncated if they are out of bounds
+	for(int i = 0; i < dim; i++){
+		if(v[i] > upperBound){
+			v[i] = upperBound;
+		} 
+		else if (v[i] < lowerBound){
+			v[i] = lowerBound;
+		}
+	}
+	
+	iter = (iter + 1) % (10000 * dim);
+}
+
 
 //Crossover as random selection from parent and larva
 /*void Combine(const vector<double> & parent, vector<double> & larva){
@@ -74,7 +120,7 @@ void Mutate(vector<double> & v){
 }*/
 
 //Crossover as random arithmetic crossover operator
-void Combine(const vector<double> & parent, vector<double> & larva){
+/*void Combine(const vector<double> & parent, vector<double> & larva){
 	double alpha = Random::get(0.0, 1.0);
 	
 	for(int i = 0; i < dim; i++){
@@ -82,16 +128,37 @@ void Combine(const vector<double> & parent, vector<double> & larva){
 	}
 		
 }
+*/
+
+//Crossover as chaotic aritmetic operator
+void Combine(const vector<double> & parent, vector<double> & larva){
+	//variable for chaotic sequence
+	static double alpha = Random::get(-1.0, 1.0);
+	
+	for(int i = 0; i < dim; i++){
+		alpha = 0.5 + 0.5 * sin(20 / alpha);
+		larva[i] = parent[i] * alpha + (1-alpha)*larva[i];
+	}
+		
+}
 
 
-vector<double> Reproduce(const vector<double> & parent){
+pair<vector<double>, vector<double>> Reproduce(const vector<double> & parent){
+	
+	pair<vector<double>, vector<double>> result;
 	vector<double> bud = parent;
 	
 	Mutate(bud);
 	
+	//first element is the "larva" (only mutation)
+	result.first = bud;
+	
 	Combine(parent, bud);
 	
-	return bud;
+	//second element is the "bud" (mutation + crossover)
+	result.second = bud;
+	
+	return result;
 }
 
 int main() {
@@ -115,16 +182,29 @@ int main() {
 		
 		while (evals < 10000*dim) {
 			// parent reproduces a bud
-			vector<double> bud = Reproduce(parent);
+			pair<vector<double>, vector<double>> result = Reproduce(parent);
+			
+			// fitness of larva is calculated
+			double fit_larva = cec17_fitness(&result.first[0]);
+			evals++;
 			
 			// fitness of bud is calculated
-			double fit_bud = cec17_fitness(&bud[0]);
+			double fit_bud = cec17_fitness(&result.second[0]);
 			evals++;
-			// if bud is better than parent, it replaces parent, otherwise it is discarded
+			
+			// best of larva, bud and parent stays, the other two are discarded
+			if(fit_larva < fitness) {
+				fitness = fit_larva;
+				parent = result.first;
+			}
+			
+			// best of larva, bud and parent stays, the other two are discarded
 			if(fit_bud < fitness) {
 				fitness = fit_bud;
-				parent = bud;
+				parent = result.second;
 			}
+			
+			
 		}
 
 		cout <<"Best ARO[F" <<funcid <<"]: " << scientific <<cec17_error(fitness) <<endl;
